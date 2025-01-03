2a83a5736f5cb4510451860b52c6df90b5ccad90
arpipatel-ap <arpipatel96@gmail.com>
2025-01-03 11:38:38 -0500

    add example of bigint

--- test1.js
+++ test1.js
@@ -1,6 +1,9 @@
 let number = 33;
 
 let stringNumber = String(number);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+const bigNumber = 245564864568747n
+
+console.log(typeof bigNumber); // typeof bigint
