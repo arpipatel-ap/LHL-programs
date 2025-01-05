a1a86d3e157527c9f937a68c05e80c7d9a97e306
arpipatel-ap <arpipatel96@gmail.com>
2025-01-05 18:55:09 -0500

    add string method example which is most confused one

--- test1.js
+++ test1.js
@@ -7,3 +7,20 @@ let stringNumber = String(number);
 const bigNumber = 245564864568747n
 
 console.log(typeof bigNumber); // typeof bigint
+
+
+//Differace bitween three methods of strings
+
+const myString = "Every green bus drives fast.";
+
+// Using length property and charAt() method
+const lengthWay = myString.charAt(myString.length - 2);
+console.log(lengthWay); // 't'
+
+// Using slice() method
+const sliceWay = myString.slice(-2, -1);
+console.log(sliceWay); // 't'
+
+// Using at() method
+const atWay = myString.at(-2);
+console.log(atWay); // 't'
