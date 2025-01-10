e8002d5750ffdbdde52f680879981585bb8d0c6b
arpipatel-ap <arpipatel96@gmail.com>
2025-01-10 11:49:38 -0500

    add replce method example

--- test1.js
+++ test1.js
@@ -43,3 +43,15 @@ const mood = 'Happy! ';
 
 console.log(`I feel ${mood.repeat(3)}`);
 // Expected output: "I feel Happy! Happy! Happy! "
+
+
+/// string method replace()
+
+const paragraph = "I think Ruth's dog is cuter than your dog!";
+
+console.log(paragraph.replace("Ruth's", 'my'));
+// Expected output: "I think my dog is cuter than your dog!"
+
+const regex = /Dog/i;
+console.log(paragraph.replace(regex, 'ferret'));
+// Expected output: "I think Ruth's ferret is cuter than your dog!"
