06411f86e756610c5187386a48ca3aa7c294b1b9
arpipatel-ap <arpipatel96@gmail.com>
2025-01-31 23:43:13 -0500

    add valueof method

--- README.md
+++ README.md
@@ -70,3 +70,5 @@ trimRight()
 trimStart()
 
 trimLeft()
+
+valueOf()
--- test1.js
+++ test1.js
@@ -138,3 +138,11 @@ console.log(greeting.trim());
 
 // console.log(greeting.trimStart());
 // // Expected output: "Hello world!   ";
+
+const stringObj = new String('foo');
+
+console.log(stringObj);
+// Expected output: String { "foo" }
+
+console.log(stringObj.valueOf());
+// Expected output: "foo"
